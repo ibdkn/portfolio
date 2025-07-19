333c49311eb4fb0f54275304470f1f62b1b3af65
irinabudiakina <ibudiakina@gmail.com>
2025-07-19 17:28:48 +0300

    added about-me

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import {HeaderComponent} from './pages/header/header.component';
 import {MainComponent} from './pages/main/main.component';
 import {SkillsComponent} from './pages/skills/skills.component';
 import {ExperienceComponent} from './pages/experience/experience.component';
+import {AboutComponent} from './pages/about/about.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, MainComponent, SkillsComponent, ExperienceComponent],
+  imports: [HeaderComponent, MainComponent, SkillsComponent, ExperienceComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
